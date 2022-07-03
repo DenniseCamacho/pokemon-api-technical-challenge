d28bde84de43eff091f1dedde28eac84cfa30de1
New Author Name <dennise.e.camacho@gmail.com>
2022-07-02 21:31:41 -0500

    attempt at adding images...

--- js/main.js
+++ js/main.js
@@ -1,24 +1,73 @@
 "use strict";
-$(document).ready(function () {
-    //jQuery ajax req.
-    //https://pokeapi.co/api/v2/pokemon/{id or name}/
-    $.get('https://pokeapi.co/api/v2/pokemon', {
-        // Calling any API endpoint without a resource ID or name will return a paginated list of available resources for that API. By default, a list "page" will contain up to 20 resources. If you would like to change this just add a 'limit' query parameter to the GET request, e.g. ?limit=60. You can use 'offset' to move to the next page, e.g. ?limit=60&offset=60.
-        //what data are you getting?
-        limit: 12
-    }).done(function (data) {
-        //do this with the data
-        console.log(data);
-        data.results.forEach((value, index, array)=>{
-            console.log(value.name);
-        let theSpan = document.getElementById('information01');
-        theSpan.insertAdjacentHTML("afterbegin", ` ${value.name} `);
+
+let showImage = {
+    doShowImage: (url) => {
+        const urlObjLinkInfo = url;
+
+        fetch(urlObjLinkInfo).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+           console.log(data.sprites.front_default);
+        }).catch(function (jqXhr, status, error) {
+            console.log(jqXhr);
+            console.log(status);
+            console.log(error);
+        });
+    }
+}
+let showData = {
+    doShowData: (data) => {
+        data.forEach((results, index, array) => {
+           let theName = results.name;
+           let url = results.url;
+           let idk = showImage.doShowImage(url);
+           let element = document.getElementById('information01');
+           element.insertAdjacentHTML('beforeend', `
+           ${theName}<br>
+           <img src="${idk}"><br>
+           `)
         });
 
-    }).fail(function (jqXhr, status, error) {
-        console.log(jqXhr, 'is jqxhr');
-        console.log(status, 'is status');
-        console.log(error, 'is error');
-    });
+    }
+}
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+// https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0
+// https://pokeapi.co/api/v2/pokemon/ditto
 
+const pokemonUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+fetch(pokemonUrl).then(function (response) {
+    return response.json();
+}).then(function (data) {
+    // console.log(data.results);
+    // console.log(data.results[0].name);
+    // console.log(data.results[0].url);
+    showData.doShowData(data.results);
+}).catch(function (jqXhr, status, error) {
+    console.log(jqXhr);
+    console.log(status);
+    console.log(error);
 });
+
+//
+//     //jQuery ajax req.
+//     //https://pokeapi.co/api/v2/pokemon/{id or name}/
+//     $.get('https://pokeapi.co/api/v2/pokemon', {
+//         // Calling any API endpoint without a resource ID or name will return a paginated list of available resources for that API. By default, a list "page" will contain up to 20 resources. If you would like to change this just add a 'limit' query parameter to the GET request, e.g. ?limit=60. You can use 'offset' to move to the next page, e.g. ?limit=60&offset=60.
+//         //what data are you getting?
+//         limit: 12,
+//         offset: 34
+//     }).done(function (data) {
+//         //do this with the data
+//         showData.doShowData(data);
+//         //     console.log(value.name);
+//         // let theSpan = document.getElementById('information01');
+//         // theSpan.insertAdjacentHTML("afterbegin", ` ${value.name} `);
+//
+//     }).fail(function (jqXhr, status, error) {
+//         console.log(jqXhr, 'is jqxhr');
+//         console.log(status, 'is status');
+//         console.log(error, 'is error');
+//     });
+//
+// });
