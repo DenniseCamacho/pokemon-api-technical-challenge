7d91e5e87c5f318dd736b54a1f8fb898320d8c37
New Author Name <dennise.e.camacho@gmail.com>
2022-07-08 16:07:41 -0500

    changing functions back

--- js/main.js
+++ js/main.js
@@ -1,42 +1,54 @@
 "use strict";
 const POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon';
 const POKEMON_IMAGE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export
 
 
-let showData = {
-    doShowData: (data) => {
-        data.forEach((results, index, array) => {
-            let theName = results.name;
-            // let url = results.url;
-            let element = document.getElementById('information01');
-            // fetchMeThenLog(url);
-            {
-                element.insertAdjacentHTML('beforeend', `
+
+
+
+function doShowData(data) {
+    data.results.forEach((results, index, array) => {
+        let theName = results.name;
+        // let url = results.url;
+        let element = document.getElementById('information01');
+        // fetchMeThenLog(url);
+        {
+            element.insertAdjacentHTML('beforeend', `
            <div class="divExerciseCSS2js">
            <img src="${POKEMON_IMAGE_URL}${index + 1}.png" alt="An image of the pokemon, ${theName}">
            ${theName}
            </div>
            `)
-            }
-        });
+        }
+    });
 
-    }
 }
 
-
-fetch(POKEMON_URL).then(function (response) {
-    return response.json();
-}).then(function (data) {
-    // console.log(data.results);
-    // console.log(data.results[0].name);
-    // console.log(data.results[0].url);
-    showData.doShowData(data.results);
-}).catch(function (jqXhr, status, error) {
-    console.log(jqXhr);
-    console.log(status);
-    console.log(error);
+$.ajax({
+    url: POKEMON_URL,
+    type: "GET",
+    dataType: "json",
+    success: function(data){
+        console.log(data);
+        doShowData(data);
+    }
 });
+
+
+// fetch(POKEMON_URL).then(function (response) {
+//     return response.json();
+// }).then(function (data_from_url) {
+//     // console.log(data.results);
+//     // console.log(data.results[0].name);
+//     // console.log(data.results[0].url);
+//     doShowData(data_from_url);
+// }).catch(function (jqXhr, status, error) {
+//     console.log(jqXhr);
+//     console.log(status);
+//     console.log(error);
+// });
 
 
 
